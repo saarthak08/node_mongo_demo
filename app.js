496fb99dda4b649abac1415235a6606785a62ec9
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-03-23 00:23:34 +0530

    dockerize the app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ else {
   port=3000;
 }
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true,useUnifiedTopology: true  });
+mongoose.connect('mongodb://mongo:27017/test', { useNewUrlParser: true,useUnifiedTopology: true  });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
